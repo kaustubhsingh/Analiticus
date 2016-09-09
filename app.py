9dbb13d320ecab8e45709d729363ef3c647adec7
Kaustubh <kaustubh.singh@gmail.com>
2016-09-08 23:13:41 -0300

    update

--- app.py
+++ app.py
@@ -25,6 +25,14 @@ app = Flask(__name__)
 
 import email
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'smajik23@gmail.com'
+app.config['MAIL_PASSWORD'] = 'smajiksmajik'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+
 #app.logger.addHandler(logging.StreamHandler())
 #app.logger.setLevel(logging.ERROR)
     
