5ad4464617a68da15a3f75f1412f9e3104a5c7ca
Kaustubh <kaustubh.singh@gmail.com>
2016-09-08 22:42:59 -0300

    update

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ mail = Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'smajik23@gmail.com'
+app.config['MAIL_USERNAME'] = 'smajik23'
 app.config['MAIL_PASSWORD'] = 'smajikforyou'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
