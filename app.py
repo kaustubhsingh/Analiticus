1a3a7cd2d99e15c818e34629cdcf45aa7af5f867
Kaustubh <kaustubh.singh@gmail.com>
2016-09-08 23:20:56 -0300

    update

--- app.py
+++ app.py
@@ -25,6 +25,15 @@ app = Flask(__name__)
 
 import email
 
+txt = open(email)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'smajik23@gmail.com'
+app.config['MAIL_PASSWORD'] = txt.read()
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
 
 
 
