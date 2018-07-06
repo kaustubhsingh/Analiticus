fb8e76bd6f77fd0e3a82755c5dd83172f3a79684
Kaustubh Singh <kaustubh.singh@gmail.com>
2018-07-06 11:39:49 -0400

    update

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def home():
         max_tweets = 200
         
         try:
-            searched_tweets = [status for status in tweepy.Cursor(api.search, q=query, lang='en', tweet_mode=extended).items(max_tweets)]
+            searched_tweets = [status for status in tweepy.Cursor(api.search, q=query, lang='en').items(max_tweets)]
                 
             #print dir(searched_tweets[0])
             pos_score = 0
